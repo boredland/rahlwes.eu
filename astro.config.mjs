1b782b7eca62fee25395a553eb5f71d253012613
Jonas Strassel <info@jonas-strassel.de>
2024-03-27 11:23:45 +0100

    :technologist: Set up cf pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import icon from "astro-icon";
-import vercel from '@astrojs/vercel/serverless';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    imageService: true,
-    devImageService: true,
-    edgeMiddleware: true,
+  adapter: cloudflare({
+    runtime: {
+      mode: "local"
+    }
   }),
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
