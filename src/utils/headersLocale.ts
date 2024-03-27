685af95f26700e3003188bea9d8f3028c4f6691b
Jonas Strassel <info@jonas-strassel.de>
2024-03-27 15:44:14 +0100

    :fire: Remove headers locale

--- src/utils/headersLocale.ts
+++
@@ -1,7 +0,0 @@
-import acceptLanguage from "accept-language";
-
-export const getHeadersLocale = (acceptLanguageHeader = "en") => {
-  acceptLanguage.languages(["en", "de"]);
-  const locale = acceptLanguage.get(acceptLanguageHeader);
-  return locale as "en" | "de";
-};
