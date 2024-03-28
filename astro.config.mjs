5cec642f4ee65593273b94bcde68d43435ce5668
Jonas Strassel <info@jonas-strassel.de>
2024-03-28 18:47:03 +0100

    :bug: Preserve suffixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,6 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: "compile",
   }),
-  build: {
-    format: 'file'
-  },
   trailingSlash: 'ignore',
   compressHTML: true,
   integrations: [
