e99f76b9bc69080c7e77f39f0074fdfe59ec6a71
Jonas Strassel <info@jonas-strassel.de>
2024-03-31 19:25:30 +0200

    ✨ Add all fields to tina

--- tina/config.ts
+++ tina/config.ts
@@ -32,6 +32,15 @@ export default defineConfig({
         path: "src/content/posts",
         fields: [
           {
+            name: "lang",
+            label: "Language",
+            type: "string",
+            options: [
+              { label: "English", value: "en" },
+              { label: "German", value: "de" },
+            ],
+          },
+          {
             type: "string",
             name: "title",
             label: "Title",
@@ -39,10 +48,38 @@ export default defineConfig({
             required: true,
           },
           {
+            name: "pubDate",
+            label: "Publication Date",
             type: "datetime",
-            name: "posted",
-            label: "Date Posted",
             required: true,
+          },
+          {
+            name: "description",
+            label: "Description",
+            type: "string",
+            required: true,
+            description: "A short preview of the post",
+          },
+          {
+            name: "author",
+            label: "Author",
+            type: "string",
+            required: true,
+          },
+          {
+            name: "image",
+            label: "Image",
+            type: "image",
+            required: true,
+            description: "URL to the post's main image",
+          },
+          {
+            name: "tags",
+            label: "Tags",
+            type: "string",
+            options: ["exhibit", "museum", "research", "inclusion", "storytelling", "education", "tutorial"],
+            required: true,
+            list: true,
           },
           {
             type: "rich-text",
