0db8cee4a7c35559a45559942a52e6ee1cd9b88f
Jonas Strassel <info@jonas-strassel.de>
2024-04-01 14:11:01 +0200

    :globe_with_meridians: Add i18next for proper interpolation support

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,5 +1,8 @@
+import i18next from 'i18next'
 import en from '../locales/en.json'
 import de from '../locales/de.json'
+import type { TranslationFunction, TranslationFunctionArgs, TranslationKeys } from 'translations'
+
 const locales = ['en', 'de'] as const
 export type Locale = (typeof locales)[number]
 
@@ -11,17 +14,31 @@ const translations = {
 export const getLangFromUrl = (url: URL) => {
   const [, lang] = url.pathname.split('/')
   if (lang in locales) return lang as Locale
-  return "en" as const
+  return 'en' as const
 }
 
-export const useTranslations = (lang: Locale = "de") => {
-  return function t<Key extends  keyof (typeof translations)["en"]>(key: Key) {
-    const dict = translations[lang] ?? translations.en;
-    return dict[key]
+type Value<Key extends TranslationKeys> = (typeof translations.en)[Key]
+
+export const useTranslations = (lng: Locale = 'de') => {
+  return <Key extends TranslationKeys>(...args: TranslationFunctionArgs<Key>) => {
+    const dict = translations[lng] ?? translations.en
+    i18next.init({
+      lng,
+      resources: {
+        [lng]: {
+          translation: dict,
+        },
+      },
+    })
+
+    return i18next.t(args[0], {
+      returnObjects: true,
+      ...(args.at(1) as Record<string, string> | undefined),
+    }) as Value<Key>
   }
 }
 
-export const getCurrentLocalizedPath = (url: URL, lang: Locale = "de") => {
+export const getCurrentLocalizedPath = (url: URL, lang: Locale = 'de') => {
   const path = url.pathname.split('/')
   path[1] = lang
   return path.join('/')
@@ -29,6 +46,5 @@ export const getCurrentLocalizedPath = (url: URL, lang: Locale = "de") => {
 
 export const getStaticPaths = () => {
   const paths = [{ params: { lang: 'en' } }, { params: { lang: 'de' } }]
-
   return paths
 }
